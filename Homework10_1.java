c2b62f3167f23bc6bcff22f9dd9d18a5133a1bf3
Benjarat Burapajetrak <85210845+geneyeans@users.noreply.github.com>
2022-03-11 09:27:32 +0700

    Add files via upload

---
+++ Homework10_1.java
@@ -0,0 +1,12 @@
+package homework10;
+
+public class Homework10_1 {
+
+public static void main(String[] args) {
+	for (int number = 1; number <= 10; number ++) {
+		if (number == 2)
+			continue;
+		System.out.println("Count: " + number);
+}
+}
+}
---
+++ Homework9_1.java
@@ -0,0 +1,15 @@
+package homework9;
+
+public class Homework9_1 {
+
+public static void main(String[] args) {
+	for (int count = 0; count <= 20; count ++) {
+		if (count == 11)
+			continue;
+		System.out.println("Count: " + count);
+
+	}
+}
+}
+
+
